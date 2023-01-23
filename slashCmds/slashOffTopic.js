f418ca42d0472e0b6aef9714b6748abc79f532e0
DESKTOP-I5GVDVP\admin <bietchetliena@gmail.com>
2023-01-23 23:37:21 +0700

    off

--- slashCmds/slashOffTopic.js
+++ slashCmds/slashOffTopic.js
@@ -13,7 +13,15 @@ module.exports = {
             let setName= 'off';
             getTime2(client, interaction, command, setName);
 
-            await interaction.reply('https://cdn.discordapp.com/attachments/960469353319526400/1034409090962702417/off.png');
+            let ranNum = Math.floor((Math.random() * 3) + 1);
+            if (ranNum == 1) {
+                await interaction.reply('https://cdn.discordapp.com/attachments/960469353319526400/1034409090962702417/off.png');
+            } else if (ranNum == 2){
+                await interaction.reply('https://cdn.discordapp.com/attachments/653718872360222760/1067119158355046410/off2.png');
+            } else{
+                await interaction.reply('https://cdn.discordapp.com/attachments/653718872360222760/1067119158577336471/off3.png');
+            }
+
         } catch (error) {
             console.log(error);
             let channel2 = client.channels.cache.get('959120662188933191');
